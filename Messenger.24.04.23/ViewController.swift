9fb5144d0530dfa4596c2449da9f2f8c28a25fbc
ZalkarbekArapbaev <zalkarbek2022@gmail.com>
2023-04-24 19:29:23 +0600

    Initial Commit

---
+++ Messenger.24.04.23/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger.24.04.23
+//
+//  Created by zalkarbek on 24/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
